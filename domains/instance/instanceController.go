58270db55e0a162834e69cc17ac609bda5f17a6a
Dev-Beom <youngbeom0908@gmail.com>
2021-12-30 02:36:42 +0900

    Chore: Todo 메모

---
+++ domains/instance/instanceController.go
@@ -0,0 +1,7 @@
+package instance
+
+/**
+todo 3 layer 구조 완성
+instance 관련 end point 정리
+validation 코드 추가
+*/
---
+++ domains/instance/service.go
@@ -0,0 +1,12 @@
+package instance
+
+type Service struct {
+	/**
+	todo 비즈니스 로직 구현
+	인스턴스 생성, 삭제, 설명 변경, 상태 변경
+	*/
+}
+
+type service struct {
+	instanceRepository Repository
+}
