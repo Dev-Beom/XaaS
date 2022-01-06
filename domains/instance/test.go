6c94a408d4d8088d29f0e56a0feac4affdf4651b
youngbeom0908 <youngbeom0908@gmail.com>
2022-01-06 19:39:26 +0900

    Refactor: E2E 테스트로 변경

---
+++ domains/instance/test.go
@@ -0,0 +1,14 @@
+package instance
+
+import (
+	"github.com/dev-beom/faas/models"
+	"github.com/labstack/echo"
+	"testing"
+)
+
+var mockDB = make(map[string]models.Instance)
+
+func TestCreateInstance(t *testing.T) {
+	e := echo.New()
+	req
+}
