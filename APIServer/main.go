8da9b45fa66cdcc14092cdda78da65156897383d
Dev-Beom <youngbeom0908@gmail.com>
2022-01-07 20:12:15 +0900

    Chore: IPC 통신 기본 코드

--- APIServer/main.go
+++ APIServer/main.go
@@ -2,14 +2,29 @@ package main
 
 import (
 	"github.com/dev-beom/xaas/apiserver/domains/instance"
+	ipc "github.com/james-barrow/golang-ipc"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"strconv"
+	"time"
 )
 
 func main() {
 	app := echo.New()
 	port := 5000
+
+	ipcServer, err := ipc.StartServer("api-server", nil)
+	if err != nil {
+		return
+	}
+	for {
+
+		ipcServer.Write(5, []byte("Hello Client 1"))
+		ipcServer.Write(7, []byte("Hello Client 2"))
+		ipcServer.Write(9, []byte("Hello Client 3"))
+
+		time.Sleep(time.Second)
+	}
 
 	instanceRepository := instance.NewRepository()
 	instanceService := instance.NewService(instanceRepository)
@@ -24,4 +39,7 @@ func main() {
 	app.DELETE("/api/instance/:id", instanceController.Delete)
 
 	app.Logger.Fatal(app.Start(":" + strconv.Itoa(port)))
+	_ = ipcServer.Write(1, []byte("Message from server"))
+	_ = ipcServer.Write(1, []byte("Message from server"))
+	_ = ipcServer.Write(1, []byte("Message from server"))
 }
--- ControllerManager/main.go
+++ ControllerManager/main.go
@@ -1,4 +1,34 @@
 package main
 
+import (
+	ipc "github.com/james-barrow/golang-ipc"
+	"log"
+)
+
 func main() {
+	ipcClient, err := ipc.StartClient("api-server", nil)
+	if err != nil {
+		return
+	}
+	for {
+		data, err := ipcClient.Read()
+
+		if err != nil {
+			break
+		}
+
+		if data.MsgType == -1 { // message type -1 is status change
+			log.Println("Status: " + data.Status)
+		}
+
+		if data.MsgType == -2 { // message type -2 is an error, these won't automatically cause the recieve channel to close.
+			log.Println("Error: " + err.Error())
+		}
+
+		if data.MsgType > 0 { // all message types above 0 have been recieved over the connection
+
+			log.Println(" Message type: ", data.MsgType)
+			log.Println("Client recieved: " + string(data.Data))
+		}
+	}
 }
