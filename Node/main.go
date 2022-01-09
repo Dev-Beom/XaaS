db548f653d154c6a65948a745eb788f18889af77
Dev-Beom <youngbeom0908@gmail.com>
2022-01-10 04:54:48 +0900

    Refactor: Node Name Logging

--- Node/main.go
+++ Node/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
 )
 
 func main() {
 	// Todo Node functions -> The file that's currently mounted.
+	nodeName := os.Getenv("NODE_NAME")
 	for _ = range time.Tick(time.Second * 10) {
-		fmt.Println(time.Now())
+		fmt.Println(nodeName, ":", time.Now())
 	}
 }
