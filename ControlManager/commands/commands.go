d9540e947daf6b59295fabad358339cb9c0fc2f8
Dev-Beom <youngbeom0908@gmail.com>
2022-01-13 23:57:14 +0900

    Chore: 문구 변경

--- ControlManager/commands/commands.go
+++ ControlManager/commands/commands.go
@@ -6,21 +6,22 @@ import (
 )
 
 func BuildNodeDockerImage() {
-	utils.Logging("이미지를 생성합니다.")
+	utils.Logging("이미지 생성 작업이 시작되었습니다.")
 	utils.RunCommand("docker", "build", "-t", constants.NodeDockerImageName, constants.NodeDockerfilePath)
-	utils.Logging("이미지 생성이 완료되었습니다.")
+	utils.Logging("이미지 생성 작업이 완료되었습니다.")
 }
 
+// RunNodeDockerImage todo -v option 을 통한 mount 처리
 func RunNodeDockerImage(nodeName string) {
 	env := "NODE_NAME=" + nodeName
-	// todo -v option 을 통한 mount 처리
-	utils.Logging(nodeName + "컨테이너를 실행합니다.")
+	utils.Logging(nodeName + " 컨테이너 실행 작업이 시작되었습니다.")
 	utils.RunCommand("docker", "run", "-d", "--name", nodeName, "-e", env, constants.NodeDockerImageName)
+	utils.Logging(nodeName + " 컨테이너 실행 작업이 완료되었습니다.")
 }
 
+// DeleteNodeContainer todo 다양한 docker rm option 을 활용한 제거, volume 관련
 func DeleteNodeContainer(nodeName string) {
-	// todo 다양한 docker rm option 을 활용한 제거, volume 관련
-	utils.Logging(nodeName + "컨테이너를 제거합니다.")
+	utils.Logging(nodeName + "컨테이너 제거 작업이 시작되었습니다.")
 	utils.RunCommand("docker", "rm", "-f", nodeName)
-	utils.Logging(nodeName + "컨테이너를 제거가 완료되었습니다.")
+	utils.Logging(nodeName + "컨테이너 제거 작업이 완료되었습니다.")
 }
