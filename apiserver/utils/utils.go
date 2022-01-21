87088bd839ca8a1f88a8bc58dd07f5c45c72c955
Dev-Beom <youngbeom0908@gmail.com>
2022-01-21 22:23:35 +0900

    Chore: 파일 확장자 추출 기능

--- apiserver/utils/utils.go
+++ apiserver/utils/utils.go
@@ -3,8 +3,10 @@ package utils
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func InterfaceToBuffer(model interface{}) (bytes.Buffer, error) {
@@ -14,6 +16,14 @@ func InterfaceToBuffer(model interface{}) (bytes.Buffer, error) {
 		return bytes.Buffer{}, err
 	}
 	return *buffer, nil
+}
+
+func GetFileExtension(filename string) (string, error) {
+	slice := strings.Split(filename, ".")
+	if len(slice) < 1 {
+		return "", errors.New("파일의 확장자를 추출할 수 없습니다")
+	}
+	return slice[len(slice)-1], nil
 }
 
 func LoggingByTest(msg string, code int, body string) {
